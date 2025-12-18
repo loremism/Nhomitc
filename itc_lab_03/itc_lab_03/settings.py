e139c0d27c8a1c77d5f0abf76877e21ae1a2af65
duykhang <yourname@gmail.com>
2025-12-18 16:14:52 +0700

    dkhang

--- itc_lab_03/itc_lab_03/settings.py
+++ itc_lab_03/itc_lab_03/settings.py
@@ -11,10 +11,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -37,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +62,7 @@ ROOT_URLCONF = 'itc_lab_03.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- itc_lab_03/myapp/views.py
+++ itc_lab_03/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'myapp/index.html')
+def about(request):
+    return render(request, 'myapp/about.html')
+def contact(request):
+    return render(request, 'myapp/contact.html')
+def shop(request):
+    return render(request, 'myapp/shop.html')
+def shopSingle(request):
+    return render(request, 'myapp/shop_single.html')
